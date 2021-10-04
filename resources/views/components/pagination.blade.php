a23c866233a6b947637ce296ee4515afd4f153a6
Emilio Gozo <emil.gozo@gmail.com>
2021-10-04 12:59:33 +0800

    style: fix for tailwind migration

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -1,19 +1,26 @@
-@if($paginator->hasPages())
+@if ($paginator->hasPages())
     <nav>
-        <ul class="pagination pull-right">
+        <ul class="flex h-10 font-medium rounded-full bg-gray-200">
             {{-- Previous Page Link --}}
-            @if($paginator->currentPage() > 5)
-                <li class="page-item">
-                    <a class="page-link" href="<?php echo $paginator->url( $paginator->currentPage() - 5 ); ?>" rel="prev" aria-label="&lsaquo; Skip 5"> &lsaquo; Skip 5 </a>
-                </li>
-            @endif
             @if ($paginator->onFirstPage())
-                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                    <span class="page-link" aria-hidden="true">&lsaquo;</span>
+                <li class="h-10 w-10 mr-1 flex justify-center items-center rounded-full bg-gray-200" aria-disabled="true"
+                    aria-label="@lang('pagination.previous')">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="feather feather-chevron-left w-6 h-6">
+                        <polyline points="15 18 9 12 15 6"></polyline>
+                    </svg>
                 </li>
             @else
-                <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
+                <li class="h-10 w-10 mr-1 flex justify-center items-center rounded-full bg-gray-200 cursor-pointer">
+                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev"
+                        aria-label="@lang('pagination.previous')">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="feather feather-chevron-left w-6 h-6">
+                            <polyline points="15 18 9 12 15 6"></polyline>
+                        </svg>
+                    </a>
                 </li>
             @endif
 
@@ -21,16 +28,22 @@
             @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                    <li class="w-10 md:flex justify-center items-center hidden leading-5 transition duration-150 ease-in rounded-full"
+                        aria-disabled="true">
+                        {{ $element }}</li>
                 @endif
 
                 {{-- Array Of Links --}}
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                            <li class="w-10 md:flex justify-center items-center hidden cursor-pointer leading-5 transition duration-150 ease-in rounded-full bg-blue-600 text-white"
+                                aria-current="page">{{ $page }}</li>
                         @else
-                            <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                            <li
+                                class="w-10 md:flex justify-center items-center hidden cursor-pointer leading-5 transition duration-150 ease-in rounded-full">
+                                <a href="{{ $url }}">{{ $page }}</a>
+                            </li>
                         @endif
                     @endforeach
                 @endif
@@ -38,17 +51,23 @@
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+                <li class="h-10 w-10 ml-1 flex justify-center items-center rounded-full bg-gray-200 cursor-pointer">
+                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="feather feather-chevron-right w-6 h-6">
+                            <polyline points="9 18 15 12 9 6"></polyline>
+                        </svg>
+                    </a>
                 </li>
             @else
-                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                    <span class="page-link" aria-hidden="true">&rsaquo;</span>
-                </li>
-            @endif
-            @if($paginator->lastPage() >= $paginator->currentPage()+5)
-                <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->url( $paginator->currentPage() + 5 ) }}" rel="prev" aria-label="Skip 5  &rsaquo;">Skip 5 &rsaquo;</a>
+                <li class="h-10 w-10 ml-1 flex justify-center items-center rounded-full bg-gray-200"
+                    aria-disabled="true" aria-label="@lang('pagination.next')">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="feather feather-chevron-right w-6 h-6">
+                        <polyline points="9 18 15 12 9 6"></polyline>
+                    </svg>
                 </li>
             @endif
         </ul>
