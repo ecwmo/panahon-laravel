425d5180c608cdde55cb6b8183df40bf8773f9be
Emilio Gozo <emil.gozo@gmail.com>
2022-01-18 17:17:51 +0800

    chore: upgrade packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-    purge: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
-    darkMode: false, // or 'media' or 'class'
+    content: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
     theme: {
-        extend: {},
-    },
-    variants: {
         extend: {},
     },
     plugins: [],
