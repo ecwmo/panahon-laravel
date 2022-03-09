c0002e4959edbb53ab3d592caa7dfa76ba8fa9ba
Emilio Gozo <emil.gozo@gmail.com>
2022-03-09 15:19:36 +0800

    fix: get APP_SUBURL from env

--- app/helpers.php
+++ app/helpers.php
@@ -5,28 +5,31 @@ use Illuminate\Support\HtmlString;
 
 function vite_assets(): HtmlString
 {
-    $devServerIsRunning = false;
-    $hostIP = env('HOST_IP', 'localhost');
-    $vitePort = env('VITE_PORT', '3000');
+  $devServerIsRunning = false;
+  $hostIP = env('HOST_IP', 'localhost');
+  $vitePort = env('VITE_PORT', '3000');
+  $subURL = env('APP_SUBURL', '');
 
-    if (app()->environment('local')) {
-        Http::get("http://{$hostIP}:{$vitePort}");
-        $devServerIsRunning = true;
-    }
+  if (strlen($subURL) > 0) $subURL = "/{$subURL}";
 
-    if ($devServerIsRunning) {
-        return new HtmlString(<<<HTML
+  if (app()->environment('local')) {
+    Http::get("http://{$hostIP}:{$vitePort}");
+    $devServerIsRunning = true;
+  }
+
+  if ($devServerIsRunning) {
+    return new HtmlString(<<<HTML
             <script type="module" src="http://{$hostIP}:{$vitePort}/@vite/client"></script>
             <script type="module" src="http://{$hostIP}:{$vitePort}/resources/scripts/app.ts"></script>
         HTML);
-    }
+  }
 
-    $manifest = json_decode(file_get_contents(
-        public_path('build/manifest.json')
-    ), true);
+  $manifest = json_decode(file_get_contents(
+    public_path('build/manifest.json')
+  ), true);
 
-    return new HtmlString(<<<HTML
-        <script type="module" src="/build/{$manifest['resources/scripts/app.js']['file']}"></script>
-        <link rel="stylesheet" href="/build/{$manifest['resources/scripts/app.ts']['css'][0]}">
+  return new HtmlString(<<<HTML
+        <script type="module" src="{$subURL}/build/{$manifest['resources/scripts/app.ts']['file']}"></script>
+        <link rel="stylesheet" href="{$subURL}/build/{$manifest['resources/scripts/app.ts']['css'][0]}">
     HTML);
 }
