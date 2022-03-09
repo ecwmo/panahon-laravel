1c29ec682ec9e194ba90cb9591ebc2f9b3a37164
Emilio Gozo <emil.gozo@gmail.com>
2022-03-09 14:25:41 +0800

    feat: improve string to float conversion - MISSING_VALUE aware - limit to 2 significant figures

--- app/Http/Controllers/API/GLabsController.php
+++ app/Http/Controllers/API/GLabsController.php
@@ -17,200 +17,230 @@ use App\Http\Controllers\Controller;
 
 class GLabsController extends Controller
 {
-    public function index(Request $request)
-    {
-        $msg = "Nothing to do";
+  var $MISSING_VALUE = 999.9;
 
-        $accessToken = $request->query('access_token');
-        $subNum = $request->query('subscriber_number');
-        $code = $request->query('code');
+  private function toFloat($strVal, $cf = 1.0, $returnRaw = false)
+  {
+    $val = (float)$strVal;
+    if ($returnRaw) return $val;
+    if ($val == $this->MISSING_VALUE) return null;
+    return round($val * $cf, 2);
+  }
 
-        # Opt-in via Webform
-        if ($code) {
-            $response = Http::post('https://developer.globelabs.com.ph/oauth/access_token', [
-                'app_id' => env('GLABS_APP_ID'),
-                'app_secret' => env('GLABS_APP_SECRET'),
-                'code' => $code
-            ]);
-            if (!$response->ok() || Arr::has($response->json(), 'error')) {
-                return response()->json(['message'=>'Invalid code']);
-            }
-            $bdy = $response->json();
-            $accessToken = $bdy['access_token'];
-            $subNum = $bdy['subscriber_number'];
-        }
+  public function index(Request $request)
+  {
+    $msg = "Nothing to do";
 
-        # Opt-in via SMS
-        if ($subNum && $accessToken) {
-            $msg = "Number already registered";
+    $accessToken = $request->query('access_token');
+    $subNum = $request->query('subscriber_number');
+    $code = $request->query('code');
 
-            # Test if token is already used
-            if(GLabs::where('access_token', $accessToken)->first()) {
-                $msg = "Error: Token already used";
-                return response()->json(['message'=>$msg]);
-            }
-
-            $station = ObservationsStation::where('mobile_number', '63'.$subNum)->first();
-
-            if (!$station) { # Create station object
-                $station = ObservationsStation::create([
-                    'name'=>'63'.$subNum,
-                    'mobile_number'=>'63'.$subNum,
-                    'station_type'=>'SMS',
-                    'status'=>'ACTIVE',
-                ]);
-            } else {
-                $station->status = 'ACTIVE';
-                $station->save();
-            }
-            $stGLabsSubs = $station->glabs_subscription;
-            if (!$stGLabsSubs) { # Create new subscription
-                $stGLabsSubs = $station->glabs_subscription()->create(['access_token'=>$accessToken]);
-                $msg = "Station registered successfuly";
-            } else { # Update token
-                $stGLabsSubs->access_token = $accessToken;
-                $stGLabsSubs->save();
-                $msg = "Station token changed";
-            }
-        }
-
-        return response()->json(['message'=>$msg]);
+    # Opt-in via Webform
+    if ($code) {
+      $response = Http::post('https://developer.globelabs.com.ph/oauth/access_token', [
+        'app_id' => env('GLABS_APP_ID'),
+        'app_secret' => env('GLABS_APP_SECRET'),
+        'code' => $code
+      ]);
+      if (!$response->ok() || Arr::has($response->json(), 'error')) {
+        return response()->json(['message' => 'Invalid code']);
+      }
+      $bdy = $response->json();
+      $accessToken = $bdy['access_token'];
+      $subNum = $bdy['subscriber_number'];
     }
 
-    public function post(Request $request)
-    {
-        # Message Received
-        # Parse and store to the database
-        if (Arr::has($request->post(), 'inboundSMSMessageList')) {
-            $senderAddress = $request->post('inboundSMSMessageList')['inboundSMSMessage'][0]['senderAddress'];
-            $subNum = Str::substr($senderAddress, 5); #63XXXXXXXXXX
-            $smsMsg = $request->post('inboundSMSMessageList')['inboundSMSMessage'][0]['message'];
+    # Opt-in via SMS
+    if ($subNum && $accessToken) {
+      $msg = "Number already registered";
 
-            $varArray = explode("+", Str::of($smsMsg)->trim()->replaceMatches('/%20/', '+'));
-            $varCount = count($varArray);
+      # Test if token is already used
+      if (GLabs::where('access_token', $accessToken)->first()) {
+        $msg = "Error: Token already used";
+        return response()->json(['message' => $msg]);
+      }
 
-            if ($subNum) {
-                $station = ObservationsStation::where('mobile_number', $subNum)->first();
-            } else {
-                return response()->json(['message'=>'Number not found']);
-            }
+      $station = ObservationsStation::where('mobile_number', '63' . $subNum)->first();
 
-            $logType = 2;
-
-            if ($varCount == 23) {
-                [$_,
-                    $tempC, $relHum, $airPresHPA, $windSpeedKPH, $windGustKPH, $windDirDeg,
-                    $solRadWPM2, $dewPtC, $windChillC, $rainTip, $rainCumTip,
-                    $voltPB1, $voltPB2, $curr, $boostPB1, $boostPB2,
-                    $curMon, $gsmSignalStrength, $arqTemp, $arqRH, $flashPg,
-                    $dateTimeStr] = $varArray;
-            } elseif ($varCount == 19) {
-                $logType = 1;
-                [$_,
-                    $tempC, $relHum, $airPresHPA, $windSpeedKPH, $windGustKPH, $windDirDeg,
-                    $solRadWPM2, $dewPtC, $windChillC, $rainTip, $rainCumTip,
-                    $arqTemp, $arqRH, $gsmSignalStrength,
-                    $voltPB1, $boostPB1, $flashPg,
-                    $dateTimeStr] = $varArray;
-
-                $voltPB1 = explode("#", $voltPB1)[0];
-            } elseif ($varCount == 24) {
-                [$_,
-                    $tempC, $relHum, $airPresHPA, $windSpeedKPH, $windGustKPH, $_, $windDirDeg,
-                    $solRadWPM2, $dewPtC, $windChillC, $rainTip, $rainCumTip,
-                    $voltPB1, $voltPB2, $curr, $boostPB1, $boostPB2,
-                    $curMon, $gsmSignalStrength, $arqTemp, $arqRH, $flashPg,
-                    $dateTimeStr] = $varArray;
-            } else {
-                Log::debug('[GlobeLabs] Invalid data. sender:'.$subNum.' message:'.$smsMsg);
-                if ($station) {
-                    $station->health()->create([
-                        'message' => $smsMsg,
-                        'timestamp' => Carbon::now()
-                    ]);
-                }
-                return response()->json(['message'=>"Error: Message not understood", 'data'=>$smsMsg]);
-            }
-
-            if ($logType==1) {
-                $dateTime = Carbon::createFromFormat("y:m:d:H:i:s" ,$dateTimeStr, 'Asia/Manila');
-            } else {
-                if (strlen($dateTimeStr)==13) {
-                    $dateTime = Carbon::createFromFormat("ymd/His" ,$dateTimeStr, 'Asia/Manila');
-                } else {
-                    $dateTime = Carbon::createFromFormat("Ymd/His" ,$dateTimeStr, 'Asia/Manila');
-                }
-            }
-
-            if ($station) {
-                $record = $station->observationRaw()->firstWhere('timestamp', $dateTime);
-                if (is_null($record)) {
-                    $obsRaw = [
-                        'temp'=>(float)$tempC,
-                        'rh'=>(float)$relHum,
-                        'pres'=>(float)$airPresHPA,
-                        'wspd'=>(float)$windSpeedKPH/3.6,
-                        'wspdx'=>(float)$windGustKPH/3.6,
-                        'wdir'=>(float)$windDirDeg,
-                        'srad'=>(float)$solRadWPM2,
-                        'td'=>(float)$dewPtC,
-                        'wchill'=>(float)$windChillC,
-                        'rr'=>(float)$rainTip*0.2*6.0,
-                        'timestamp'=>$dateTime
-                    ];
-                    $station->observationRaw()->create($obsRaw);
-                    $station->observation()->create($obsRaw);
-
-                    $stnHealth = [
-                        'vb1'=>(float)$voltPB1,
-                        'bp1'=>(float)$boostPB1,
-                        'temp_arq'=>(float)$arqTemp,
-                        'rh_arq'=>(float)$arqRH,
-                        'ss'=>(int)$gsmSignalStrength,
-                        'fpm'=>(int)$flashPg,
-                        'message'=>$smsMsg,
-                        'timestamp'=>$dateTime
-                    ];
-                    if ($logType==2) {
-                        $stnHealth = Arr::add($stnHealth, 'vb2', (float)$voltPB2);
-                        $stnHealth = Arr::add($stnHealth, 'bp2', (float)$boostPB2);
-                        $stnHealth = Arr::add($stnHealth, 'curr', (float)$curr);
-                        $stnHealth = Arr::add($stnHealth, 'cm', $curMon);
-                    }
-                    $station->health()->create($stnHealth);
-
-                    Log::debug('[GlobeLabs] Data saved. sender:'.$subNum.' message:'.$smsMsg);
-                    return response()->json(['message'=>"Data saved"]);
-                } else {
-                    Log::debug('[GlobeLabs] Data not saved, record already exists. sender:'.$subNum.' message:'.$smsMsg);
-                    return response()->json(['message'=>"Data not saved, record already exists"]);
-                }
-            } else {
-                Log::debug('[GlobeLabs] unknown sender:'.$subNum.' message:'.$smsMsg);
-                return response()->json(['message'=>"Error: No associated weather station for ".$subNum]);
-            }
-        }
-        # Stop Subscription
-        # Delete database entry
-        elseif (Arr::has($request->post(), 'unsubscribed')) {
-            $subNum = $request->post('unsubscribed')['subscriber_number'];
-            if ($subNum) {
-                $station = ObservationsStation::where('mobile_number', "63".$subNum)->firstOrFail();
-
-                $stGLabsSubs = $station->glabs_subscription;
-                if ($stGLabsSubs) { # Delete token
-                    $stGLabsSubs->delete();
-                    return response()->json(['message'=>"Unsubscribed"]);
-                }
-            }
-            Log::debug('[GlobeLabs] Unsubscribe:'.$subNum);
-            return response()->json(['message'=>"Nothing to do"]);
-        }
-        elseif (Arr::has($request->post(), 'error')) {
-            Log::debug('[GlobeLabs] Error:'.$request->post('error'));
-            return response()->json(['error'=>$request->post('error')]);
-        }
-
-        return response()->json(['message'=>"Nothing to do"]);
+      if (!$station) { # Create station object
+        $station = ObservationsStation::create([
+          'name' => '63' . $subNum,
+          'mobile_number' => '63' . $subNum,
+          'station_type' => 'SMS',
+          'status' => 'ACTIVE',
+        ]);
+      } else {
+        $station->status = 'ACTIVE';
+        $station->save();
+      }
+      $stGLabsSubs = $station->glabs_subscription;
+      if (!$stGLabsSubs) { # Create new subscription
+        $stGLabsSubs = $station->glabs_subscription()->create(['access_token' => $accessToken]);
+        $msg = "Station registered successfuly";
+      } else { # Update token
+        $stGLabsSubs->access_token = $accessToken;
+        $stGLabsSubs->save();
+        $msg = "Station token changed";
+      }
     }
+
+    return response()->json(['message' => $msg]);
+  }
+
+  public function post(Request $request)
+  {
+    # Message Received
+    # Parse and store to the database
+    if (Arr::has($request->post(), 'inboundSMSMessageList')) {
+      $senderAddress = $request->post('inboundSMSMessageList')['inboundSMSMessage'][0]['senderAddress'];
+      $subNum = Str::substr($senderAddress, 5); #63XXXXXXXXXX
+      $smsMsg = $request->post('inboundSMSMessageList')['inboundSMSMessage'][0]['message'];
+
+      $varArray = explode("+", Str::of($smsMsg)->trim()->replaceMatches('/%20/', '+'));
+      $varCount = count($varArray);
+
+      if ($subNum) {
+        $station = ObservationsStation::where('mobile_number', $subNum)->first();
+      } else {
+        return response()->json(['message' => 'Number not found']);
+      }
+
+      $logType = 2;
+
+      if ($varCount == 23) {
+        [
+          $_,
+          $tempC, $relHum, $airPresHPA, $windSpeedKPH, $windGustKPH, $windDirDeg,
+          $solRadWPM2, $dewPtC, $windChillC, $rainTip, $rainCumTip,
+          $voltPB1, $voltPB2, $curr, $boostPB1, $boostPB2,
+          $curMon, $gsmSignalStrength, $arqTemp, $arqRH, $flashPg,
+          $dateTimeStr
+        ] = $varArray;
+      } elseif ($varCount == 19) {
+        $logType = 1;
+        [
+          $_,
+          $tempC, $relHum, $airPresHPA, $windSpeedKPH, $windGustKPH, $windDirDeg,
+          $solRadWPM2, $dewPtC, $windChillC, $rainTip, $rainCumTip,
+          $arqTemp, $arqRH, $gsmSignalStrength,
+          $voltPB1, $boostPB1, $flashPg,
+          $dateTimeStr
+        ] = $varArray;
+
+        $voltPB1 = explode("#", $voltPB1)[0];
+      } elseif ($varCount == 24) {
+        [
+          $_,
+          $tempC, $relHum, $airPresHPA, $windSpeedKPH, $windGustKPH, $_, $windDirDeg,
+          $solRadWPM2, $dewPtC, $windChillC, $rainTip, $rainCumTip,
+          $voltPB1, $voltPB2, $curr, $boostPB1, $boostPB2,
+          $curMon, $gsmSignalStrength, $arqTemp, $arqRH, $flashPg,
+          $dateTimeStr
+        ] = $varArray;
+      } else {
+        Log::debug('[GlobeLabs] Invalid data. sender:' . $subNum . ' message:' . $smsMsg);
+        if ($station) {
+          $station->health()->create([
+            'message' => $smsMsg,
+            'timestamp' => Carbon::now()
+          ]);
+        }
+        return response()->json(['message' => "Error: Message not understood", 'data' => $smsMsg]);
+      }
+
+      if ($logType == 1) {
+        $dateTime = Carbon::createFromFormat("y:m:d:H:i:s", $dateTimeStr, 'Asia/Manila');
+      } else {
+        if (strlen($dateTimeStr) == 13) {
+          $dateTime = Carbon::createFromFormat("ymd/His", $dateTimeStr, 'Asia/Manila');
+        } else {
+          $dateTime = Carbon::createFromFormat("Ymd/His", $dateTimeStr, 'Asia/Manila');
+        }
+      }
+
+      if ($station) {
+        $record = $station->observationRaw()->firstWhere('timestamp', $dateTime);
+        if (is_null($record)) {
+          $obsRaw = [
+            'temp' => $this->toFloat($tempC, 1.0, true),
+            'rh' => $this->toFloat($relHum, 1.0, true),
+            'pres' => $this->toFloat($airPresHPA, 1.0, true),
+            'wspd' => $this->toFloat($windSpeedKPH, 1.0, true),
+            'wspdx' => $this->toFloat($windGustKPH, 1.0, true),
+            'wdir' => $this->toFloat($windDirDeg, 1.0, true),
+            'srad' => $this->toFloat($solRadWPM2, 1.0, true),
+            'td' => $this->toFloat($dewPtC, 1.0, true),
+            'wchill' => $this->toFloat($windChillC, 1.0, true),
+            'rr' => $this->toFloat($rainTip, 1.0, true),
+            'timestamp' => $dateTime
+          ];
+
+          $obs = [
+            'temp' => $this->toFloat($tempC),
+            'rh' => $this->toFloat($relHum),
+            'pres' => $this->toFloat($airPresHPA, 1.0, true),
+            'wspd' => $this->toFloat($windSpeedKPH, 1.0 / 3.6),
+            'wspdx' => $this->toFloat($windGustKPH, 1.0 / 3.6),
+            'wdir' => $this->toFloat($windDirDeg),
+            'srad' => $this->toFloat($solRadWPM2),
+            'td' => $this->toFloat($dewPtC),
+            'wchill' => $this->toFloat($windChillC),
+            'rr' => $this->toFloat($rainTip, 0.2 * 6.0),
+            'timestamp' => $dateTime
+          ];
+
+          $station->observationRaw()->create($obsRaw);
+          $station->observation()->create($obs);
+
+          $stnHealth = [
+            'vb1' => (float)$voltPB1,
+            'bp1' => (float)$boostPB1,
+            'temp_arq' => (float)$arqTemp,
+            'rh_arq' => (float)$arqRH,
+            'ss' => (int)$gsmSignalStrength,
+            'fpm' => (int)$flashPg,
+            'message' => $smsMsg,
+            'timestamp' => $dateTime
+          ];
+          if ($logType == 2) {
+            $stnHealth = Arr::add($stnHealth, 'vb2', (float)$voltPB2);
+            $stnHealth = Arr::add($stnHealth, 'bp2', (float)$boostPB2);
+            $stnHealth = Arr::add($stnHealth, 'curr', (float)$curr);
+            $stnHealth = Arr::add($stnHealth, 'cm', $curMon);
+          }
+          $station->health()->create($stnHealth);
+
+          Log::debug('[GlobeLabs] Data saved. sender:' . $subNum . ' message:' . $smsMsg);
+          return response()->json(['message' => "Data saved"]);
+        } else {
+          Log::debug('[GlobeLabs] Data not saved, record already exists. sender:' . $subNum . ' message:' . $smsMsg);
+          return response()->json(['message' => "Data not saved, record already exists"]);
+        }
+      } else {
+        Log::debug('[GlobeLabs] unknown sender:' . $subNum . ' message:' . $smsMsg);
+        return response()->json(['message' => "Error: No associated weather station for " . $subNum]);
+      }
+    }
+    # Stop Subscription
+    # Delete database entry
+    elseif (Arr::has($request->post(), 'unsubscribed')) {
+      $subNum = $request->post('unsubscribed')['subscriber_number'];
+      if ($subNum) {
+        $station = ObservationsStation::where('mobile_number', "63" . $subNum)->firstOrFail();
+
+        $stGLabsSubs = $station->glabs_subscription;
+        if ($stGLabsSubs) { # Delete token
+          $stGLabsSubs->delete();
+          return response()->json(['message' => "Unsubscribed"]);
+        }
+      }
+      Log::debug('[GlobeLabs] Unsubscribe:' . $subNum);
+      return response()->json(['message' => "Nothing to do"]);
+    } elseif (Arr::has($request->post(), 'error')) {
+      Log::debug('[GlobeLabs] Error:' . $request->post('error'));
+      return response()->json(['error' => $request->post('error')]);
+    }
+
+    return response()->json(['message' => "Nothing to do"]);
+  }
 }
