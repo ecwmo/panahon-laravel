ad0727060e153194a259ce735f7b9551cda3a768
Emilio Gozo <emil.gozo@gmail.com>
2022-05-02 14:21:14 +0800

    chore: remove 'time_error' column

--- database/migrations/2021_06_30_021558_create_observations_stationhealth_table.php
+++ database/migrations/2021_06_30_021558_create_observations_stationhealth_table.php
@@ -6,48 +6,47 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateObservationsStationHealthTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('observations_stationhealth', function (Blueprint $table) {
-            $table->id();
-            $table->double('vb1')->nullable()->comment('Voltage Power Board 1 (V)');
-            $table->double('vb2')->nullable()->comment('Voltage Power Board 2 (V)');
-            $table->double('curr')->nullable()->comment('Current (A)');
-            $table->double('bp1')->nullable()->comment('Boost Power Board 1 (V)');
-            $table->double('bp2')->nullable()->comment('Boost Power Board 2 (V)');
-            $table->string('cm', 255)->nullable()->comment('Current Monitoring');
-            $table->integer('ss')->nullable()->comment('GSM Signal Strength');
-            $table->double('temp_arq')->nullable()->comment('Temperature of ARQ');
-            $table->double('rh_arq')->nullable()->comment('Humidity of ARQ');
-            $table->string('fpm', 255)->nullable()->comment('Flash Page (memory)');
-            $table->text('error_msg')->nullable();
-            $table->text('message')->nullable();
-            $table->integer('data_count')->nullable();
-            $table->string('data_status', 255)->nullable();
-            $table->integer('minutes_difference')->nullable();
-            $table->string('time_error', 255)->nullable();
-            $table->dateTimeTz('timestamp');
-            $table->foreignId('station_id')
-                ->constrained('observations_station')
-                ->onUpdate('cascade')
-                ->onDelete('cascade');
-            $table->unique(['station_id', 'timestamp']);
-            $table->timestampsTz();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('observations_stationhealth', function (Blueprint $table) {
+      $table->id();
+      $table->double('vb1')->nullable()->comment('Voltage Power Board 1 (V)');
+      $table->double('vb2')->nullable()->comment('Voltage Power Board 2 (V)');
+      $table->double('curr')->nullable()->comment('Current (A)');
+      $table->double('bp1')->nullable()->comment('Boost Power Board 1 (V)');
+      $table->double('bp2')->nullable()->comment('Boost Power Board 2 (V)');
+      $table->string('cm', 255)->nullable()->comment('Current Monitoring');
+      $table->integer('ss')->nullable()->comment('GSM Signal Strength');
+      $table->double('temp_arq')->nullable()->comment('Temperature of ARQ');
+      $table->double('rh_arq')->nullable()->comment('Humidity of ARQ');
+      $table->string('fpm', 255)->nullable()->comment('Flash Page (memory)');
+      $table->text('error_msg')->nullable();
+      $table->text('message')->nullable();
+      $table->integer('data_count')->nullable();
+      $table->string('data_status', 255)->nullable();
+      $table->integer('minutes_difference')->nullable();
+      $table->dateTimeTz('timestamp');
+      $table->foreignId('station_id')
+        ->constrained('observations_station')
+        ->onUpdate('cascade')
+        ->onDelete('cascade');
+      $table->unique(['station_id', 'timestamp']);
+      $table->timestampsTz();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('observations_stationhealth');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('observations_stationhealth');
+  }
 }
