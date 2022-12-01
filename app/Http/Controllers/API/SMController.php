265590639bc5927972c58f4de1b4e86cf995bf68
Emilio Gozo <emil.gozo@gmail.com>
2022-12-01 11:12:24 +0800

    feat: SMController

---
+++ app/Http/Controllers/API/SMController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Support\Facades\Log;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+use App\Models\GLabs;
+use App\Models\ObservationsStation;
+
+use App\Http\Controllers\Controller;
+
+class SMController extends Controller
+{
+    public function index(Request $request)
+    {
+        Log::debug('[SM] GET: ' . json_encode($request));
+
+        return 'SMGet';
+    }
+
+    public function post(Request $request)
+    {
+        Log::debug('[SM] POST: ' . json_encode($request));
+
+        return 'SMPost';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\API\RoleController;
 use App\Http\Controllers\API\ObservationsStationController;
 use App\Http\Controllers\API\GLabsController;
 use App\Http\Controllers\API\GLabsLoadController;
+use App\Http\Controllers\API\SMController;
 
 Route::name('api.')->group(function () {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
@@ -20,6 +21,9 @@ Route::name('api.')->group(function () {
     Route::get('/globelabs', [GLabsController::class, 'index'])->name('glabs.index');
     Route::post('/globelabs', [GLabsController::class, 'post'])->name('glabs.post');
     Route::post('/globelabs/load', [GLabsLoadController::class, 'post'])->name('glabs.load');
+
+    Route::get('/sm', [SMController::class, 'index'])->name('sm.index');
+    Route::post('/sm', [SMController::class, 'post'])->name('sm.post');
 
     Route::middleware('auth:api')->group(function () {
         Route::post('logout', [AuthController::class, 'logout'])->name('logout');
